bdfcc99e636348c21e3b8ef7961849fa9a5ce732
CataMM8 <cmatamorosm@hotmail.com>
2021-10-22 20:59:02 -0600

    tarea4

---
+++ Scripts/script.js
@@ -0,0 +1,30 @@
+return (
+<button id="toggleBloque" onclick="toggleSection()">Mostrar Operaciones</button>
+)
+
+<div><id="operacionesContenedor">
+    <label for="myNumber1">Numero 1:</label>
+    <input type="number" id="myNumber1">
+    <label for="myNumber2">Numero 2:</label>
+    <input type="number" id="myNumber2">
+    <div class="operaciones">
+    <button onclick="sumar()">Sumar</button>
+    <button onclick="restar()">Restar</button>
+    <button onclick="multiplicar()">Multiplicar</button>
+</div>
+
+
+<div>
+    <p id="result"></p>
+</div>
+
+<div> <class="operaciones">
+    <button onclick="sumar()">Sumar</button>
+    <button onclick="restar()">Restar</button>
+    <button onclick="multiplicar()">Multiplicar</button>
+</div>
+
+<div>
+<p id="result"></p>
+</div> 
+)
